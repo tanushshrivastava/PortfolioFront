245ff878534158d1e42e4ffe0440ced1160c3c7c
Tanush Shrivastava <tanus@MacBook-Pro-3.local>
2025-02-24 00:16:05 -0600

    cursor removal

--- src/app/App.js
+++ src/app/App.js
@@ -7,7 +7,6 @@ import {
 import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
-import AnimatedCursor  from "../hooks/AnimatedCursor";
 import "./App.css";
 
 function _ScrollToTop(props) {
@@ -23,14 +22,6 @@ export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <div className="cursor__dot">
-        <AnimatedCursor
-          innerSize={15}
-          outerSize={15}
-          color="255, 255 ,255"
-          outerAlpha={0.4}
-          innerScale={0.7}
-          outerScale={5}
-        />
       </div>
       <ScrollToTop>
         <Headermain />
